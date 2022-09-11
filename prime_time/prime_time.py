5211024180bbc90fe12d58cee10b221ed2a2c635
atharva <ath284.ap@gmail.com>
2022-09-11 20:33:54 +0530

    prime time done

--- prime_time/prime_time.py
+++ prime_time/prime_time.py
@@ -1,86 +1,130 @@
 import socket
-import selectors
-import types
 import json
-
-sel = selectors.DefaultSelector()
-
-def accept_wrapper(sock: socket.socket):
-    conn, addr = sock.accept()
-    print(f"accepted connection from {addr}")
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-
-def isprime(num):
-    if type(num) == float:
-        return False
-    if num > 1:
-    # check for factors
-        for i in range(2, num):
-            if (num % i) == 0:
-                return True
-    return False
-
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print(f"closing connection to {data.addr}")
-            sel.unregister(sock)
-            sock.close()
-
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            req = json.loads(data.outb)
-            num_types = [int, float]
-            print(req)
-            if req["method"] != "isPrime" or type(req["number"]) != num_types:
-                res = {"method":"malformed request"}
-                print(res)
-                res_str = json.dumps(res)+"\n"
-                sock.sendall(res_str.encode())
-
-            number = req["number"]
-            if isprime(number):
-                res = {"method":"isPrime", "prime":True}
-                res_str = json.dumps(res)+"\n"
-                print(res)
-                sock.sendall(res_str.encode())
-            else:
-                res = {"method":"isPrime", "prime":False}
-                res_str = json.dumps(res)+"\n"
-                print(res)
-                sock.sendall(res_str.encode())
-
-            data.outb = data.outb[len(data.outb):]
+import threading
+from concurrent.futures import ThreadPoolExecutor
+from collections import defaultdict
+from itertools import count
 
 host = '0.0.0.0'
 port = 5000
 
 
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-print(f"listening on {(host, port)}")
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
+SEED = 22801763489  # One-billionth prime number
 
-try:
-    while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
+
+class PrimeTest:
+    def __init__(self):
+        self.__P = {}
+        self.__gen = self.__primes()
+        self.__top = 0
+
+    @staticmethod
+    def __primes():
+        ps = defaultdict(list)
+        for i in count(2):
+            if i not in ps:
+                yield i
+                ps[i**2].append(i)
             else:
-                service_connection(key, mask)
+                for n in ps[i]:
+                    ps[i + (n if n == 2 else 2 * n)].append(n)
+                del ps[i]
 
-except KeyboardInterrupt:
-    print("closing server")
-finally:
-    sel.close()
+    def __next_prime(self) -> int:
+        i = next(self.__gen)
+        self.__P[i] = None
+        self.__top = max(self.__top, i)
+        return i
+
+    def is_prime(self, n) -> bool:
+        # Is it a known prime?
+        if type(n) == float:
+            return False
+
+        if n in self.__P:
+            return True
+
+        # Is it a known composite?
+        if n <= self.__top and n not in self.__P:
+            return False
+
+        # Trial division
+        for p in self.__P:
+            if p * p >= n:
+                return True
+            if n % p == 0:
+                return False
+
+        # Make new primes on the fly until sqrt
+        while True:
+            p = self.__next_prime()
+            if p * p >= n:
+                return True
+            if n % p == 0:
+                return False
+
+
+pt = PrimeTest()
+
+def handle_connection(conn:socket.socket, addr):
+    print(f"connected to {addr}")
+
+    while True:
+        recv_data = b''
+        #Read the JSON request until the newline or until EOF
+        while True:
+            c = conn.recv(1)
+            if c:
+                if c == b'\n':
+                    break
+                recv_data+=c
+            else:
+                conn.close()
+                print(f"connection closed {addr}")
+                return
+
+        #Write the JSON response
+        res = {}
+
+        try:
+            j_req = json.loads(recv_data)
+            num_types = [int, float]
+            if j_req.get("method") == None or j_req.get("number") == None:
+                res["method"] = "malformed request"
+            elif j_req["method"] != "isPrime" or type(j_req["number"]) not in num_types:
+                res["method"] = "malformed request"
+            elif pt.is_prime(j_req["number"]):
+                res["method"] = "isPrime"
+                res["prime"] = True
+            else:
+                res["method"] = "isPrime"
+                res["prime"] = False
+
+        except:
+            res["method"] = "malformed request"
+            raw_response = json.dumps(res)+"\n"
+            conn.send(raw_response.encode())
+            print(res)
+            conn.close()
+            return
+
+        print(j_req)
+
+        raw_response = json.dumps(res)+"\n"
+        conn.send(raw_response.encode())
+        print(res)
+
+def main():
+    num_threads = 5
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server.bind((host, port))
+            server.listen()
+            print(f"server listening on {(host,port)}")
+            while True:
+                conn, addr = server.accept()
+                executor.submit(handle_connection, conn, addr)
+    print("server shutting down")
+
+if __name__ == '__main__':
+    main()
