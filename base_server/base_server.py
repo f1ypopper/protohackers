19c414a557de74b690628f5f7d319d9ee2b34a8c
atharva <ath284.ap@gmail.com>
2022-09-14 18:42:49 +0530

    03:Means to an End is done

--- base_server/base_server.py
+++ base_server/base_server.py
@@ -9,12 +9,15 @@ class BaseServer:
         self.num_threads = num_threads
 
     def start(self, serve_function):
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-                server.bind((self.host, self.port))
-                server.listen()
-                print(f"server listening on {(self.host, self.port)}")
-                while True:
-                    conn, addr = server.accept()
-                    executor.submit(serve_function, conn, addr)
-        print("server shutting down")
+        try:
+            with ThreadPoolExecutor(max_workers=5) as executor:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+                    server.bind((self.host, self.port))
+                    server.listen()
+                    print(f"server listening on {(self.host, self.port)}")
+                    while True:
+                        conn, addr = server.accept()
+                        executor.submit(serve_function, conn, addr)
+        except:
+            print("server closing down...")
+
--- means_to_an_end/means_to_an_end.py
+++ means_to_an_end/means_to_an_end.py
@@ -2,18 +2,18 @@ import math
 import socket
 import struct
 import sys, os, inspect
+
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 from base_server.base_server import BaseServer
-
+import bisect
 host = '0.0.0.0'
 port = 5000
 num_threads = 5
 
-def time_ascending_func(entry):
-    return entry['timestamp']
+
 
 class TimeSeriesDB:
     def __init__(self) -> None:
@@ -21,65 +21,63 @@ class TimeSeriesDB:
 
     def insert(self, timestamp, price):
         entry = {'timestamp': timestamp, 'price':price}
+        bisect.insort(self.db, entry, key=self.by_timestamp) #insert item without sorting everytime
 
-        if self.db.count() == 0:
-            self.db.append(entry)
-            return
-
-        if self.db[0]['timestamp'] >= entry['timestamp']:
-            self.db.insert(0, entry)
-            return
-
-        self.db.append(entry)
-        self.db.sort(key=time_ascending_func)
+    def by_timestamp(self, entry):
+        return entry['timestamp']
 
     def query(self, mintime, maxtime):
         aggregate = 0
         entry_count = 0
         for entry in self.db:
-            if entry['timestamp'] >= mintime and entry['timestamp'] <= maxtime:
+            if mintime <= entry['timestamp']  and entry['timestamp'] <= maxtime:
                 aggregate+=entry['price']
                 entry_count+=1
-
+        if aggregate == 0:
+            return 0
         mean = math.floor(aggregate/entry_count)
         return mean
 
-def handle_connection(conn: socket.socket, addr):
+def handle_connection(client: socket.socket, addr):
+
     print(f'connected to {addr}')
+    try:
+        db = TimeSeriesDB()
+        with client, client.makefile('rwb') as conn:
+            while True:
+                raw_msg = conn.read(9)
+                print(raw_msg)
+                if not raw_msg:
+                    conn.close()
+                    print(f'connection closed {addr}')
+                    return
 
-    db = TimeSeriesDB()
+                msg_type, val1, val2 = struct.unpack('>cii', raw_msg)
 
-    while True:
-#        raw_msg = conn.recv(9)
-#        print(raw_msg)
+                if msg_type != b'I' and  msg_type != b'Q':
+                    conn.close()
+                    print(f'connection closed {addr}')
+                    return
 
-        msg_type = conn.recv(1)
-        val1 = conn.recv(4)
-        val2 = conn.recv(4)
-        if not msg_type:
-            conn.close()
-            print(f'connection closed {addr}')
-            return
+                if msg_type == b'I':
+                    timestamp = val1
+                    price = val2
+                    entry = {'timestamp':timestamp, 'price':price}
+                    print(f'inserted {entry}')
+                    db.insert(timestamp, price)
 
-#        msg_type, val1, val2 = struct.unpack('cii', raw_msg)
-        if msg_type != b'I' and  msg_type != b'Q':
-            conn.close()
-            print(f'connection closed {addr}')
-            return
+                elif msg_type == b'Q':
+                    mintime = val1
+                    maxtime = val2
+                    aggregate = db.query(mintime, maxtime)
+                    conn.write(struct.pack('>i',aggregate))
+                    conn.flush()
+                    print(f'query result {aggregate}')
 
-        if msg_type == b'I':
-            timestamp = int.from_bytes(val1, "big")
-            price = int.from_bytes(val2, "big")
-            entry = {'timestamp':timestamp, 'price':price}
-            db.insert(entry)
-            print(f'inserted {entry}')
-
-        elif msg_type == b'Q':
-            mintime = int.from_bytes(val1, "big")
-            maxtime = int.from_bytes(val2, "big")
-            aggregate = db.query(mintime, maxtime)
-            conn.send(aggregate)
-            print(f'query result {aggregate}')
+    except Exception  as e:
+        print(f"exception occurred: {e}")
+    finally:
+        client.close()
 
 def main():
     server = BaseServer(host, port, num_threads)
