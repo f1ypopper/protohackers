f18438749881027df40a3355dd1f3fdcf5cea4bd
Atharva <ath284.ap@gmail.com>
2022-10-10 17:25:49 +0530

    still untested unusual database

--- python/unusual_database/unusual_database.py
+++ python/unusual_database/unusual_database.py
@@ -1,6 +1,7 @@
 from asyncio.log import logger
 import socket
 import logging
+from struct import pack
 HOST = '0.0.0.0'
 PORT = 5000
 
@@ -11,24 +12,33 @@ db['version'] = 'Key-DB 1.0'
 
 def parse_insert(data: str):
     return data.split('=')
+
+def format_key_value(key:str, value:str):
+    return key+"="+value
+
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server.bind((HOST, PORT))
     logging.info("server running...")
     while True:
-        conn, addr = server.accept()
-        logging.info(f"accepted conn from {addr}")
-        packet = conn.recv(999)
-        if not packet:
-            conn.close()
-            logging.info(f"connection closed {addr}")
+        packet, addr = server.recvfrom(1000)
         packet = packet.decode()
         if '=' in packet:
             key, value = parse_insert(packet)
-            if key != 'version':
+            if key != "version":
                 db[key] = value
+                logging.info(f"inserted {key}={value}")
         else:
-            conn.send(db[packet])
+            value = None
+            try:
+                value = db[packet]
+                logging.info(f"retrieved {packet}={value}")
+            except:
+                logging.info(f"key:{packet} not found")
+                value = ""
+            msg = format_key_value(packet, value)
+            server.sendto(msg.encode(), addr)
+
     server.close()
 if __name__ == '__main__':
     main()
