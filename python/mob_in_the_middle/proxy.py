80c9d28ff5eb292ae697c3d3251e98c2d73af7b6
Atharva <ath284.ap@gmail.com>
2022-10-21 09:43:48 +0530

    Added missing loop to handle

--- python/mob_in_the_middle/proxy.py
+++ python/mob_in_the_middle/proxy.py
@@ -1,4 +1,5 @@
 import asyncio
+from asyncore import write
 import logging
 import re
 
@@ -11,17 +12,26 @@ TONY_BOGUS = "7YWHMfk9JZe0LM0g1ZauHuiSxhI"
 
 async def handle(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     ureader, uwriter = await asyncio.open_connection(UPSTREAM_SERVER, UPSTREAM_PORT)
-    logging.info("connected to upstream chat server")
-    server_msg = await ureader.readline()
-    replaced_server_msg = re.sub(BOGUS_RE, TONY_BOGUS, server_msg.decode())
-    logging.info(f"[SERVER]{replaced_server_msg}")
-    writer.write(replaced_server_msg.encode())
-    await writer.drain()
-    client_msg = await reader.readline()
-    replaced_client_msg = re.sub(BOGUS_RE, TONY_BOGUS, client_msg.decode())
-    logging.info(f"[CLIENT]{replaced_client_msg}")
-    uwriter.write(replaced_client_msg.encode())
-    await uwriter.drain()
+    try:
+        while not reader.at_eof() or not ureader.at_eof():
+            server_msg = await ureader.readline()
+            replaced_server_msg = re.sub(BOGUS_RE, TONY_BOGUS, server_msg.decode())
+            logging.info(f"[SERVER]{replaced_server_msg}")
+            writer.write(replaced_server_msg.encode())
+            await writer.drain()
+            client_msg = await reader.readline()
+            replaced_client_msg = re.sub(BOGUS_RE, TONY_BOGUS, client_msg.decode())
+            logging.info(f"[CLIENT]{replaced_client_msg}")
+            uwriter.write(replaced_client_msg.encode())
+            await uwriter.drain()
+    except ValueError:
+        pass
+    finally:
+        await uwriter.drain()
+        await writer.drain()
+        writer.close()
+        uwriter.close()
+        logging.info("connection closed") 
 
 async def main():
     logging.basicConfig(level=logging.DEBUG)
