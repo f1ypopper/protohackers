c5dd1bcdfa7657ad283dbe2dadfa304281edbba3
Atharva <ath284.ap@gmail.com>
2022-10-23 20:51:48 +0530

    added disconnect again

--- python/mob_in_the_middle/proxy.py
+++ python/mob_in_the_middle/proxy.py
@@ -14,13 +14,17 @@ async def handle_client(client_reader: asyncio.StreamReader, client_writer: asyn
     addr = client_writer.get_extra_info('peername')
     logging.info(f"{addr} connected to the proxy server")
     proxy_reader, proxy_writer = await asyncio.open_connection(UPSTREAM_SERVER, UPSTREAM_PORT)
+    state = 'RUNNING'
+    async def disconnect():
+        state = 'STOP'
 
     async def handle_proxy_msg():
         while True:
             proxy_msg = (await proxy_reader.readline()).decode()
+            if not proxy_msg:
+                await disconnect()
             logging.info(f"{UPSTREAM_SERVER} received msg: {proxy_msg.strip()}")
-            #malicious_msg = re.sub(BOGUS_RE, TONY_BOGUS, proxy_msg)
-            malicious_msg = proxy_msg
+            malicious_msg = re.sub(BOGUS_RE, TONY_BOGUS, proxy_msg)
             client_writer.write(malicious_msg.encode())
             await client_writer.drain()
             logging.info(f"{addr} sent msg: {malicious_msg.strip()}")
@@ -28,15 +32,29 @@ async def handle_client(client_reader: asyncio.StreamReader, client_writer: asyn
     async def handle_client_msg():
         while True:
             client_msg = (await client_reader.readline()).decode()
+            if not client_msg:
+                await disconnect()
             logging.info(f"{addr} received msg: {client_msg.strip()}")
-            #malicious_msg = re.sub(BOGUS_RE, TONY_BOGUS, client_msg)
-            malicious_msg = client_msg
+            malicious_msg = re.sub(BOGUS_RE, TONY_BOGUS, client_msg)
             proxy_writer.write(malicious_msg.encode())
             await proxy_writer.drain()
             logging.info(f"{UPSTREAM_SERVER} sent msg: {malicious_msg.strip()}")
 
     client_task = asyncio.create_task(handle_client_msg())
     proxy_task = asyncio.create_task(handle_proxy_msg())
+
+    while True:
+        if state == 'STOP':
+            handle_client_msg.cancel()
+            handle_proxy_msg.cancel()
+            await asyncio.gather(client_task)
+            await asyncio.gather(proxy_task)
+            await client_writer.drain()
+            await proxy_writer.drain()
+            client_writer.close()
+            proxy_writer.close()
+            logging.info(f'{addr} disconnected')
+            break
 
 async def main():
     logging.basicConfig(level=logging.DEBUG)
