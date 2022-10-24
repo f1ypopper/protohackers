36ee9a71f8d13a5e0c01c0e3a24827e4a5f1a693
Atharva <ath284.ap@gmail.com>
2022-10-24 10:39:13 +0530

    Simplified Asyncio

--- python/mob_in_the_middle/proxy.py
+++ python/mob_in_the_middle/proxy.py
@@ -16,46 +16,59 @@ def create_mal_msg(msg: str):
     mal_msg = [re.sub(BOGUS_RE, TONY_BOGUS, word) for word in split_msg]
     return ' '.join(mal_msg)
 
+async def handle(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    while not reader.at_eof():
+        proxy_msg = (await reader.readline()).decode()
+        if not proxy_msg:
+            break
+        logging.info(f"{UPSTREAM_SERVER} received msg: {proxy_msg.strip()}")
+        malicious_msg = create_mal_msg(proxy_msg) 
+        writer.write(malicious_msg.encode())
+
+    await writer.drain()
+    writer.close()
+    logging.info("connection closed")
+
 async def handle_client(client_reader: asyncio.StreamReader, client_writer: asyncio.StreamWriter):  
     addr = client_writer.get_extra_info('peername')
     logging.info(f"{addr} connected to the proxy server")
     proxy_reader, proxy_writer = await asyncio.open_connection(UPSTREAM_SERVER, UPSTREAM_PORT)
+    asyncio.gather(handle(proxy_reader, client_writer), handle(client_reader, proxy_writer))
+#    async def handle_proxy_msg():
+#        while True:
+#            proxy_msg = (await proxy_reader.readline()).decode()
+#            logging.info(f"{UPSTREAM_SERVER} received msg: {proxy_msg.strip()}")
+#            malicious_msg = create_mal_msg(proxy_msg) 
+#            if not malicious_msg:
+#                raise asyncio.CancelledError
+#            client_writer.write(malicious_msg.encode())
+#            await client_writer.drain()
+#            logging.info(f"{addr} sent msg: {malicious_msg.strip()}")
+#
+#    async def handle_client_msg():
+#        while True:
+#            client_msg = (await client_reader.readline()).decode()
+#            malicious_msg = create_mal_msg(client_msg)
+#            if not malicious_msg:
+#                raise asyncio.CancelledError
+#            proxy_writer.write(malicious_msg.encode())
+#            await proxy_writer.drain()
+#            logging.info(f"{addr} received msg: {client_msg.strip()}")
+#            logging.info(f"{UPSTREAM_SERVER} sent msg: {malicious_msg.strip()}")
 
-    async def handle_proxy_msg():
-        while True:
-            proxy_msg = (await proxy_reader.readline()).decode()
-            logging.info(f"{UPSTREAM_SERVER} received msg: {proxy_msg.strip()}")
-            malicious_msg = create_mal_msg(proxy_msg) 
-            if not malicious_msg:
-                raise asyncio.CancelledError
-            client_writer.write(malicious_msg.encode())
-            await client_writer.drain()
-            logging.info(f"{addr} sent msg: {malicious_msg.strip()}")
-
-    async def handle_client_msg():
-        while True:
-            client_msg = (await client_reader.readline()).decode()
-            malicious_msg = create_mal_msg(client_msg)
-            if not malicious_msg:
-                raise asyncio.CancelledError
-            proxy_writer.write(malicious_msg.encode())
-            await proxy_writer.drain()
-            logging.info(f"{addr} received msg: {client_msg.strip()}")
-            logging.info(f"{UPSTREAM_SERVER} sent msg: {malicious_msg.strip()}")
-
-    client_task = asyncio.create_task(handle_client_msg())
-    proxy_task = asyncio.create_task(handle_proxy_msg())
-    try:
-        await client_task
-        await proxy_task
-    except asyncio.CancelledError:
-        client_task.cancel()
-        proxy_task.cancel()
-        await client_writer.drain()
-        await proxy_writer.drain()
-        client_writer.close()
-        proxy_writer.close()
-        logging.info("client closed")
+#    client_task = asyncio.create_task(handle_client_msg())
+#    proxy_task = asyncio.create_task(handle_proxy_msg())
+#    try:
+#        await client_task
+#        await proxy_task
+#    except asyncio.CancelledError:
+#        client_task.cancel()
+#        proxy_task.cancel()
+#        await client_writer.drain()
+#        await proxy_writer.drain()
+#        client_writer.close()
+#        proxy_writer.close()
+#        logging.info("client closed")
 
 async def main():
     logging.basicConfig(level=logging.DEBUG)
