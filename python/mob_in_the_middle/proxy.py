94c21bb2b39222f3afae029d96c54341ae67a33c
Atharva <ath284.ap@gmail.com>
2022-10-24 09:45:34 +0530

    Mob in the Middle Working

--- python/mob_in_the_middle/proxy.py
+++ python/mob_in_the_middle/proxy.py
@@ -2,29 +2,32 @@ import asyncio
 import logging
 import re
 
+#todo: better regex matching
 PROXY = "0.0.0.0"
 PROXY_PORT = 5000
 UPSTREAM_SERVER = "chat.protohackers.com"
 UPSTREAM_PORT = 16963
-BOGUS_RE = r"7\w{25,35}"
+BOGUS_RE = r"^7(\w{25,36})$"
 TONY_BOGUS = "7YWHMfk9JZe0LM0g1ZauHuiSxhI"
 
+
+def create_mal_msg(msg: str):
+    split_msg = msg.split(' ')
+    mal_msg = [re.sub(BOGUS_RE, TONY_BOGUS, word) for word in split_msg]
+    return ' '.join(mal_msg)
 
 async def handle_client(client_reader: asyncio.StreamReader, client_writer: asyncio.StreamWriter):  
     addr = client_writer.get_extra_info('peername')
     logging.info(f"{addr} connected to the proxy server")
     proxy_reader, proxy_writer = await asyncio.open_connection(UPSTREAM_SERVER, UPSTREAM_PORT)
-    state = 'RUNNING'
-    async def disconnect():
-        state = 'STOP'
 
     async def handle_proxy_msg():
         while True:
             proxy_msg = (await proxy_reader.readline()).decode()
-            if not proxy_msg:
-                await disconnect()
             logging.info(f"{UPSTREAM_SERVER} received msg: {proxy_msg.strip()}")
-            malicious_msg = re.sub(BOGUS_RE, TONY_BOGUS, proxy_msg)
+            malicious_msg = create_mal_msg(proxy_msg) 
+            if not malicious_msg:
+                raise asyncio.CancelledError
             client_writer.write(malicious_msg.encode())
             await client_writer.drain()
             logging.info(f"{addr} sent msg: {malicious_msg.strip()}")
@@ -32,29 +35,27 @@ async def handle_client(client_reader: asyncio.StreamReader, client_writer: asyn
     async def handle_client_msg():
         while True:
             client_msg = (await client_reader.readline()).decode()
-            if not client_msg:
-                await disconnect()
-            logging.info(f"{addr} received msg: {client_msg.strip()}")
-            malicious_msg = re.sub(BOGUS_RE, TONY_BOGUS, client_msg)
+            malicious_msg = create_mal_msg(client_msg)
+            if not malicious_msg:
+                raise asyncio.CancelledError
             proxy_writer.write(malicious_msg.encode())
             await proxy_writer.drain()
+            logging.info(f"{addr} received msg: {client_msg.strip()}")
             logging.info(f"{UPSTREAM_SERVER} sent msg: {malicious_msg.strip()}")
 
     client_task = asyncio.create_task(handle_client_msg())
     proxy_task = asyncio.create_task(handle_proxy_msg())
-
-    while True:
-        if state == 'STOP':
-            handle_client_msg.cancel()
-            handle_proxy_msg.cancel()
-            await asyncio.gather(client_task)
-            await asyncio.gather(proxy_task)
-            await client_writer.drain()
-            await proxy_writer.drain()
-            client_writer.close()
-            proxy_writer.close()
-            logging.info(f'{addr} disconnected')
-            break
+    try:
+        await client_task
+        await proxy_task
+    except asyncio.CancelledError:
+        client_task.cancel()
+        proxy_task.cancel()
+        await client_writer.drain()
+        await proxy_writer.drain()
+        client_writer.close()
+        proxy_writer.close()
+        logging.info("client closed")
 
 async def main():
     logging.basicConfig(level=logging.DEBUG)
